ba82a03cc5eea11af4ffec22233fd3bd3ec2fd13
marcelobiao <marcelobiao2@gmail.com>
2021-06-26 13:30:16 -0300

    Refatorando components

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,40 @@
 <template>
     <div id="app">
-        <h1>Olá Vue</h1>
+        <h1>Calculadora</h1>
+        <Calculator/>
     </div>
 </template>
 
 <script>
+import Calculator from "./main/Calculator"
 export default {
-
+    components: {Calculator}
 }
 </script>
 
 <style>
-    @font-face {
-        font-family: "RobotoMono";
-        src: url("./fonts/RobotoMono-Thin.ttf");
-    }
-    
-    * {
-        font-family: "RobotoMono", monospace;
-    }
+@font-face {
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-Thin.ttf");
+}
 
-    body{
-        margin: 0;
-    }
+* {
+    font-family: "RobotoMono", monospace;
+}
 
-    #app{
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
+body{
+    margin: 0;
+}
 
-        color: #fff;
-        background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
-    }
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
 </style>
