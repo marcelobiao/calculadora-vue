61b40bc2416e886ca769917e40d6ef1f33401518
marcelobiao <marcelobiao2@gmail.com>
2021-06-26 13:23:30 -0300

    Criando Componentes básicos da aplicação

---
+++ src/main/Calculator.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+    import Display from "../components/Display"
+    import Button from "../components/Button"
+
+    export default {
+        components: {Button, Display}
+    }
+</script>
+
+<style>
+
+</style>
